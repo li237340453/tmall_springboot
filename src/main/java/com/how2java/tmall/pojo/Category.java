0546a7c915523e9e25c2e4940d12e90a93dd5db9
li237340453 <237340453@qq.com>
2022-08-14 00:28:03 +0800

    往里面插入了数据

---
+++ src/main/java/com/how2java/tmall/pojo/Category.java
@@ -0,0 +1,39 @@
+package com.how2java.tmall.pojo;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "category")
+@JsonIgnoreProperties({"handler", "hibernateLazyInitializer"})
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    int id;
+
+    String name;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
